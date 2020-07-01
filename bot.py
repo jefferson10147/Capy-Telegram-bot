741dcfed8c0a7e8bb117871bec20a4873f4f44aa
Jefferson Montilla <jefferson.montilla98@gmail.com>
2020-07-01 19:14:56 -0400

    ramdons functions sends a photo, instead a text

--- bot.py
+++ bot.py
@@ -6,21 +6,21 @@ from request_json import get_ramdon_dog
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-TOKEN = '1122265359:AAHo8OuC5bovxR3uAgI4WWVDVV5czrIv940'
+TOKEN = '935538322:AAGJE2zDh2UeTCI2cXC6jBy-0yjdR0DDG34'
 updater = Updater(token=TOKEN, use_context=True)
 
 dispatcher = updater.dispatcher
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text = "uwu")
+    context.bot.send_message(chat_id=update.effective_chat.id, text = "hi uwu")
 
 def ramdon_cat(update,context):
     url = get_ramdon_cat()
-    context.bot.send_message(chat_id=update.effective_chat.id, text = url)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo = url)
 
 def ramdon_dog(update,context):
     url = get_ramdon_dog()
-    context.bot.send_message(chat_id = update.effective_chat.id, text =  url)
+    context.bot.send_photo(chat_id = update.effective_chat.id, photo =  url)
 
 start_handler = CommandHandler('start', start)
 ramdon_cat_handler = CommandHandler('cat',ramdon_cat)
