acce7f3d5c4d3ded489360610be4b966b798aab0
Jefferson Montilla <jefferson.montilla98@gmail.com>
2020-07-02 15:49:24 -0400

    adding credits section

--- bot.py
+++ bot.py
@@ -12,11 +12,17 @@ updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text = "hi uwu")
+    greet_sticker = 'CAACAgEAAxkBAAL_v17-Nqi0j6rUs3ed4bSxE0K4kgd-AAJ4AAMWxqQJZTrUpaRtkRgaBA'
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "*Capy says hi*")
+    context.bot.send_sticker(chat_id = update.effective_chat.id, sticker = greet_sticker)
+
+def credit(update,context):
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "Programmed by: @coquiUnet")
+    context.bot.send_message(chat_id = update.effective_chat.id, text = "Source code at: https://github.com/jefferson10147/Capy-Telegram-bot")
 
 def ramdon_cat(update,context):
     url = get_ramdon_cat()
-    context.bot.send_photo(chat_id=update.effective_chat.id, photo = url)
+    context.bot.send_photo(chat_id = update.effective_chat.id, photo = url)
 
 def ramdon_dog(update,context):
     url = get_ramdon_dog()
@@ -25,10 +31,12 @@ def ramdon_dog(update,context):
 start_handler = CommandHandler('start', start)
 ramdon_cat_handler = CommandHandler('cat',ramdon_cat)
 ramdon_dog_handler = CommandHandler('dog',ramdon_dog)
+credit_handler = CommandHandler('credits',credit)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(ramdon_cat_handler)
 dispatcher.add_handler(ramdon_dog_handler)
+dispatcher.add_handle(credit_handler)
 
 updater.start_polling()
 updater.idle()
