9de56420624f09161d71de6fddfa7ad5e88e2b47
Jefferson Montilla <jefferson.montilla98@gmail.com>
2020-07-03 00:50:49 -0400

    Adding random stickers functions

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
+import random
 import logging
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
-from request_json import get_ramdon_cat
-from request_json import get_ramdon_dog
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from request_json import get_random_cat
+from request_json import get_random_dog
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
 
 TOKEN = '935538322:AAGJE2zDh2UeTCI2cXC6jBy-0yjdR0DDG34'
 updater = Updater(token=TOKEN, use_context=True)
@@ -20,23 +23,47 @@ def credit(update,context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Programmed by: @coquiUnet")
     context.bot.send_message(chat_id = update.effective_chat.id, text = "Source code at: https://github.com/jefferson10147/Capy-Telegram-bot")
 
-def ramdon_cat(update,context):
-    url = get_ramdon_cat()
+def random_cat(update,context):
+    url = get_random_cat()
     context.bot.send_photo(chat_id = update.effective_chat.id, photo = url)
 
-def ramdon_dog(update,context):
-    url = get_ramdon_dog()
+def random_dog(update,context):
+    url = get_random_dog()
     context.bot.send_photo(chat_id = update.effective_chat.id, photo =  url)
 
+def get_random_sticker(update,context):
+    with open('./stickers/random_stickers_id.txt','r') as file:
+        lines = file.readlines()
+        sticker_id = random.choice(lines).replace('\n','')
+        context.bot.send_sticker(chat_id = update.effective_chat.id, sticker = sticker_id)
+
+def add_sticker(update,context):
+    sticker_id = ''.join([update.message.sticker.file_id,'\n'])
+    with open('./stickers/random_stickers_id.txt','a') as file:
+        file.write(sticker_id)
+        context.bot.send_message(chat_id = update.effective_chat.id, text = sticker_id)
+
+def unknow(update,context):
+    with open('./stickers/capy_stickers_id.txt','r') as file:
+        lines = file.readlines()
+        sticker_id = random.choice(lines).replace('\n','')
+        context.bot.send_sticker(chat_id = update.effective_chat.id, sticker = sticker_id)
+    
 start_handler = CommandHandler('start', start)
-ramdon_cat_handler = CommandHandler('cat',ramdon_cat)
-ramdon_dog_handler = CommandHandler('dog',ramdon_dog)
+random_cat_handler = CommandHandler('cat',random_cat)
+random_dog_handler = CommandHandler('dog',random_dog)
+random_sticker_handler = CommandHandler('sticker',get_random_sticker)
 credit_handler = CommandHandler('credits',credit)
+add_sticker_handler = MessageHandler(Filters.sticker,add_sticker)
+unknow_handler = MessageHandler(Filters.text,unknow)
 
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(ramdon_cat_handler)
-dispatcher.add_handler(ramdon_dog_handler)
+dispatcher.add_handler(random_cat_handler)
+dispatcher.add_handler(random_dog_handler)
+dispatcher.add_handler(random_sticker_handler)
 dispatcher.add_handler(credit_handler)
+dispatcher.add_handler(add_sticker_handler)
+dispatcher.add_handler(unknow_handler)
 
 updater.start_polling()
 updater.idle()
--- request_json.py
+++ request_json.py
@@ -3,13 +3,13 @@ import requests
 CAT_API = 'https://api.thecatapi.com/v1/images/search'
 DOGO_API = 'https://dog.ceo/api/breeds/image/random'
 
-def get_ramdon_cat():
+def get_random_cat():
     response = requests.get(CAT_API)
     if response.status_code == 200:
         obj= response.json()
         return obj[0]['url']  
 
-def get_ramdon_dog():
+def get_random_dog():
     response = requests.get(DOGO_API)
     if response.status_code == 200:
         obj= response.json()
